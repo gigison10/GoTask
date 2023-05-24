2e780985de9134c41a34c30d8587da3f353821ca
Your Name <eugen.izman@gmail.com>
2023-05-24 19:31:16 +0300

    deploy on gh pages

--- src/components/projectField/projectField.jsx
+++ src/components/projectField/projectField.jsx
@@ -2,20 +2,25 @@ import "./projectField.scss";
 import { Fragment } from "react";
 
 function ProjectField(props) {
-  function deleteProject() {
-    console.log("asd");
+  function handleDeleteProject(projectId) {
+    props.deleteProject(projectId);
+    // console.log(projectId);
   }
 
   return (
     <Fragment>
       {props.projectDetails.map((data) => (
-        <div className="project-field" key={data.id}>
-          <div type="submit" onClick={deleteProject} className="ckeckBlock">
+        <div className="project-field" key={data.projectId}>
+          <div
+            type="submit"
+            onClick={() => handleDeleteProject(data.projectId)}
+            className="ckeckBlock"
+          >
             <input className="check" type="checkbox" />
             <button className="deteButton">delete</button>
           </div>
-          <h5>{data.name}</h5>
-          <h5>{data.date}</h5>
+          <h5>{data.projectName}</h5>
+          <h5>{data.startingDate}</h5>
           <h5>{data.deadLine}</h5>
           <h5>In progress</h5>
         </div>
